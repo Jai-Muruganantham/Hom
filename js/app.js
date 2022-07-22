045f70c8936c89d608505ccb3e3f972e89a6dabe
Muruganantham J <46363304+Jai-Muruganantham@users.noreply.github.com>
2022-07-22 15:17:38 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ particlesJS('particles-js',
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#0a0833"
         },
         "polygon": {
           "nb_sides": 5
@@ -122,7 +122,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#000000",
+      "background_color": "#0a0833",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
