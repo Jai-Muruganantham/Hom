e69254e2f05e174efac393fa48de643f8806515a
Muruganantham J <46363304+Jai-Muruganantham@users.noreply.github.com>
2022-11-15 10:48:15 +0200

    Update script.js
     under development

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var app = document.getElementById('typed');
 
 var typewriter = new Typewriter(app, {
-    strings: ['Hello buddy 😀', 'I\'m Jai.','The Web Page is underr Development'],
+    strings: ['Hello buddy 😀', 'I\'m Jai.','The Web Page is under development'],
     autoStart: true,
     loop: true,
     delay: 100
